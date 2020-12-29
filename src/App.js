a47794267a52445c108814930535af0a2fdcfdeb
John Bruno <johnnyb@MacBook-Pro.local>
2020-12-29 11:45:30 -0600

    added unit 3 and 4 projects and some styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <h1> Welcome to my Portfolio</h1>
+        <h1>Hello, my name is John Bruno. <br/>I'm a full stack developer living in Nashville, TN</h1>
+        <br/>
+        <h1>Technical Skills</h1>
+        <h2>Javascript | Jquery | HTML5 | Node.JS | CSS3| ES6 | EJS | Mongoose| Bootstrap | Express | React | MongoDB | Heroku | GitHub | Ruby | Rails | SQL | Postgres | Postman | ​Netlify | Material UI</h2>
+        <br/>
+        <h2 className="projects-header">Recent Projects</h2>
         <div className="container">
           <div className="project">
             <Projects
@@ -26,7 +31,31 @@ class App extends React.Component {
               img={"https://i.imgur.com/AzUvG57.png"}
             />
           </div>
+          <div className="project">
+            <Projects
+              name={"Codex"}
+              git={"https://github.com/mikegoolsby/codex_client"}
+              live={"https://reverent-kalam-023988.netlify.app/"}
+              img={"https://i.imgur.com/js1IdsJ.png"}
+            />
+          </div>
+          <div className="project">
+            <Projects
+              name={"Bar Bible"}
+              git={"https://github.com/Johnnyb818/bar_bible_client"}
+              live={"https://optimistic-booth-932d5e.netlify.app/"}
+              img={"https://i.imgur.com/DYiLPil.png"}
+            />
+          </div>
         </div>
+        <br/>
+        <div>
+          <h1>John Bruno</h1>
+          <h2>Johnnyb818@gmail.com</h2>
+          <h3>615-430-8181</h3>
+
+        </div>
+
       </>
     );
   }
